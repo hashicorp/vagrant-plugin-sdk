be7be1a04d0f591b0165905fb97c79446197f460
Chris Roberts <croberts@hashicorp.com>
2020-12-09 14:42:07 -0800

    Keep the config and documentation bits disabled for now

--- core/host.go
+++ core/host.go
@@ -1,11 +1,11 @@
 package core
 
-import (
-	"github.com/hashicorp/vagrant-plugin-sdk/docs"
-)
+// import (
+// 	"github.com/hashicorp/vagrant-plugin-sdk/docs"
+// )
 
 type Host interface {
-	Config() interface{}
-	Documentation() (*docs.Documentation, error)
+	// Config() interface{}
+	// Documentation() (*docs.Documentation, error)
 	Detect() (detected bool, err error)
 }
