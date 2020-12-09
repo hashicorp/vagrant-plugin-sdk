42fd7446e34ab365a5d39cd3d3c35848fcac2d27
sophia <scastellarin95@gmail.com>
2020-12-08 18:18:24 -0600

    Machine grpc setup

--- core/machine.go
+++ core/machine.go
@@ -1,10 +1,5 @@
 package core
 
-import (
-	"github.com/hashicorp/vagrant-plugin-sdk/multistep"
-	"github.com/hashicorp/vagrant-plugin-sdk/terminal"
-)
-
 // the ssh info in vagrant core ends up dumping out
 // a bunch of options, but they are also ssh specific
 // where this would be used for other stuff too (like
@@ -19,28 +14,39 @@ type MachineState struct {
 
 type Machine interface {
 	// accessors
-	Box() (box Box, err error)
-	DataDir() (path string, err error)
-	Environment() (env Environment, err error)
-	ID() (id string, err error)
-	Name() (name string, err error)
-	Provider() (p Provider, err error)
-	// ProviderConfig
-	ProviderName() (name string, err error)
-	// Triggers
-	UI() (ui terminal.UI, err error)
-	Vagrantfile() (file Vagrantfile, err error)
+	// Box() (box Box, err error)
+	// DataDir() (path string, err error)
+	// Environment() (env Environment, err error)
+	// ID() (id string, err error)
+	// Name() (name string, err error)
+	// Provider() (p Provider, err error)
+	// // ProviderConfig
+	// ProviderName() (name string, err error)
+	// // Triggers
+	// UI() (ui terminal.UI, err error)
+	// Vagrantfile() (file Vagrantfile, err error)
 
 	// actual workers
-	Action(name string, state multistep.StateBag) (err error)
-	Communicate() (comm Communicator, err error)
-	Guest() (g Guest, err error)
-	SetID(value string) (err error)
-	IndexUUID() (id string, err error)
-	Inspect() (printable string, err error)
-	Reload() (err error)
-	ConnectionInfo() (info ConnectionInfo, err error)
-	State() (state *MachineState, err error)
-	UID() (user_id int, err error)
-	SyncedFolders() (folders []SyncedFolder, err error)
+	ActionFunc() interface{}
+	CommunicateFunc() interface{}
+	GuestFunc() interface{}
+	SetIDFunc() interface{}
+	IndexUUIDFunc() interface{}
+	InspectFunc() interface{}
+	ReloadFunc() interface{}
+	ConnectionInfoFunc() interface{}
+	StateFunc() interface{}
+	UIDFunc() interface{}
+	SyncedFoldersFunc() interface{}
+	// Action(name string, state multistep.StateBag) (err error)
+	// Communicate() (comm Communicator, err error)
+	// Guest() (g Guest, err error)
+	// SetID(value string) (err error)
+	// IndexUUID() (id string, err error)
+	// Inspect() (printable string, err error)
+	// Reload() (err error)
+	// ConnectionInfo() (info ConnectionInfo, err error)
+	// State() (state *MachineState, err error)
+	// UID() (user_id int, err error)
+	// SyncedFolders() (folders []SyncedFolder, err error)
 }
