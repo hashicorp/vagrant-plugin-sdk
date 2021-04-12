8f4895dfa9e3f834437c4131a7b5854bf47eb011
sophia <scastellarin95@gmail.com>
2021-04-12 16:25:20 -0500

    Command info

--- core/command.go
+++ core/command.go
@@ -2,11 +2,15 @@ package core
 
 import "github.com/DavidGamba/go-getoptions/option"
 
+type CommandInfo struct {
+	Name     []string
+	Help     string
+	Synopsis string
+	Flags    []*option.Option
+}
+
 type Command interface {
-	Name() ([]string, error)
-	Help() (string, error)
-	Synopsis() (string, error)
-	Flags() ([]*option.Option, error)
 	Execute(name string) (int64, error)
 	Subcommands() ([]Command, error)
+	CommandInfo(CommandInfo, error)
 }
