af2ca2a64c10c3091acc4e4cb2e470abace4fd64
sophia <scastellarin95@gmail.com>
2021-04-23 14:36:17 -0500

    Setup capability host

---
+++ core/capability_host.go
@@ -0,0 +1,22 @@
+package core
+
+// TODO: this map should have a bunch of argmapper functions
+type CapabilityHost struct {
+	capabilities map[string]func()
+}
+
+func (c *CapabilityHost) HasCapability(name string) bool {
+	if _, ok := c.capabilities[name]; ok {
+		return true
+	}
+	return false
+}
+
+func (c *CapabilityHost) Capability(name string) {
+	c.capabilities[name]()
+}
+
+func (c *CapabilityHost) RegisterCapability(name string, f func()) error {
+	c.capabilities[name] = f
+	return nil
+}
