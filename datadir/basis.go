af5c5376025cd2a796d18e0e63abd2531edb25f5
Chris Roberts <croberts@hashicorp.com>
2021-05-04 09:06:26 -0700

    Add helper to generate project datadir from basis

--- datadir/basis.go
+++ datadir/basis.go
@@ -1,5 +1,9 @@
 package datadir
 
+import (
+	"github.com/hashicorp/vagrant-plugin-sdk/helper/path"
+)
+
 type Basis struct {
 	Dir
 }
@@ -12,3 +16,16 @@ func NewBasis(path string) (*Basis, error) {
 
 	return &Basis{Dir: dir}, nil
 }
+
+// Project returns the Dir implementation scoped to a specific project.
+func (p *Basis) Project(name string) (*Project, error) {
+	dir, err := NewScopedDir(p, path.NewPath("target").Join(name).String())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Project{Dir: dir}, nil
+}
+
+// Assert implementation
+var _ Dir = (*Basis)(nil)
