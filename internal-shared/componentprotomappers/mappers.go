f2ce250017ffb352dd067d9e6fd62b2a18ad94b1
sophia <scastellarin95@gmail.com>
2021-05-04 14:55:34 -0500

    Host mapper to connect to already running host process

--- internal-shared/componentprotomappers/mappers.go
+++ internal-shared/componentprotomappers/mappers.go
@@ -2,13 +2,11 @@ package componentprotomappers
 
 import (
 	"context"
-	"io"
+	"time"
 
 	"github.com/hashicorp/go-hclog"
-	"github.com/hashicorp/go-plugin"
 	"google.golang.org/grpc"
 
-	"github.com/hashicorp/vagrant-plugin-sdk/component"
 	"github.com/hashicorp/vagrant-plugin-sdk/core"
 	plugininternal "github.com/hashicorp/vagrant-plugin-sdk/internal/plugin"
 	"github.com/hashicorp/vagrant-plugin-sdk/internal/pluginargs"
@@ -18,7 +16,6 @@ import (
 // All is the list of all mappers as raw function pointers.
 var All = []interface{}{
 	Host,
-	HostProto,
 }
 
 func Host(
@@ -27,54 +24,26 @@ func Host(
 	log hclog.Logger,
 	internal *pluginargs.Internal,
 ) (core.Host, error) {
-	// Create our plugin
 	p := &plugininternal.HostPlugin{
 		Mappers: internal.Mappers,
 		Logger:  log,
 	}
 
-	conn, err := internal.Broker.Dial(input.StreamId)
+	timeout := 5 * time.Second
+	// Create a new cancellation context so we can cancel in the case of an error
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	// Connect to the local server
+	conn, err := grpc.DialContext(ctx, input.ServerAddr,
+		grpc.WithBlock(),
+		grpc.WithInsecure(),
+	)
 	if err != nil {
 		return nil, err
 	}
 	internal.Cleanup.Do(func() { conn.Close() })
 
 	client, err := p.GRPCClient(ctx, internal.Broker, conn)
-	if err != nil {
-		return nil, err
-	}
-
-	// Our UI should implement close since we have to stop streams and
-	// such but we gate it here in case we ever change the implementation.
-	if closer, ok := client.(io.Closer); ok {
-		internal.Cleanup.Do(func() { closer.Close() })
-	}
-
 	return client.(core.Host), nil
-}
-
-func HostProto(
-	host component.Host,
-	log hclog.Logger,
-	internal *pluginargs.Internal,
-) *vagrant_plugin_sdk.Args_Host {
-	// Create our plugin
-	p := &plugininternal.HostPlugin{
-		Impl:    host,
-		Mappers: internal.Mappers,
-		Logger:  log,
-	}
-
-	id := internal.Broker.NextId()
-
-	// Serve it
-	go internal.Broker.AcceptAndServe(id, func(opts []grpc.ServerOption) *grpc.Server {
-		server := plugin.DefaultGRPCServer(opts)
-		if err := p.GRPCServer(internal.Broker, server); err != nil {
-			panic(err)
-		}
-		return server
-	})
-
-	return &vagrant_plugin_sdk.Args_Host{StreamId: id}
 }
