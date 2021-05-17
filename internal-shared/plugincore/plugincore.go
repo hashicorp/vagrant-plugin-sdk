f8c92ba1c774846b2ff16e5f83149301c96e04a7
Chris Roberts <croberts@hashicorp.com>
2021-05-17 16:32:42 -0700

    Add shared plugin core so we can build core plugins

---
+++ internal-shared/plugincore/plugincore.go
@@ -0,0 +1,49 @@
+package plugincore
+
+import (
+	"github.com/hashicorp/go-argmapper"
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+
+	"github.com/hashicorp/vagrant-plugin-sdk/core"
+	coreplugin "github.com/hashicorp/vagrant-plugin-sdk/internal/plugin/core"
+)
+
+func NewTargetPlugin(impl core.Target, log hclog.Logger) plugin.GRPCPlugin {
+	return &coreplugin.TargetPlugin{
+		Impl:    impl,
+		Mappers: coreMappers,
+		Logger:  log,
+	}
+}
+
+func NewProjectPlugin(impl core.Project, log hclog.Logger) plugin.GRPCPlugin {
+	return &coreplugin.ProjectPlugin{
+		Impl:    impl,
+		Mappers: coreMappers,
+		Logger:  log,
+	}
+}
+
+func NewBasisPlugin(impl core.Basis, log hclog.Logger) plugin.GRPCPlugin {
+	return &coreplugin.BasisPlugin{
+		Impl:    impl,
+		Mappers: coreMappers,
+		Logger:  log,
+	}
+}
+
+var (
+	coreMappers = []*argmapper.Func{}
+)
+
+func init() {
+	for _, f := range coreplugin.MapperFns {
+		fn, err := argmapper.NewFunc(f)
+		if err != nil {
+			panic(err)
+		}
+
+		coreMappers = append(coreMappers, fn)
+	}
+}
