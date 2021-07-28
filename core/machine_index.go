92d5ff7bc8951335cad054dca235e5dc2de4c2fb
sophia <scastellarin95@gmail.com>
2021-07-28 17:26:23 -0500

    Fill in target index implementation

--- core/machine_index.go
+++
@@ -1,9 +0,0 @@
-package core
-
-type TargetIndex interface {
-	Delete(machine Machine) (err error)
-	Get(uuid string) (entry Machine, err error)
-	Includes(uuid string) (exists bool, err error)
-	Set(entry Machine) (updatedEntry Machine, err error)
-	Recover(entry Machine) (updatedEntry Machine, err error)
-}
--- internal/plugin/core/target_index.go
+++ internal/plugin/core/target_index.go
@@ -66,34 +66,92 @@ type targetIndexServer struct {
 	vagrant_plugin_sdk.UnimplementedTargetIndexServiceServer
 }
 
-func (t *targetIndexClient) Delete(machine core.Machine) (err error) {
-	id, err := machine.ID()
-	_, err = t.client.Delete(t.ctx, &vagrant_plugin_sdk.Ref_Target{
-		ResourceId: id,
-	})
+func (t *targetIndexClient) Delete(target core.Target) (err error) {
+	targetArg, err := t.Map(
+		target,
+		(*vagrant_plugin_sdk.Args_Target)(nil),
+		argmapper.Typed(t.ctx),
+	)
+	if err != nil {
+		return err
+	}
+	_, err = t.client.Delete(t.ctx, targetArg.(*vagrant_plugin_sdk.Args_Target))
 	return
 }
 
-func (t *targetIndexClient) Get(uuid string) (entry core.Machine, err error) {
-	return
+func (t *targetIndexClient) Get(uuid string) (entry core.Target, err error) {
+	target, err := t.client.Get(
+		t.ctx,
+		&vagrant_plugin_sdk.TargetIndex_GetRequest{Uuid: uuid},
+	)
+	if err != nil {
+		return nil, err
+	}
+	m, err := t.Map(
+		target,
+		(*core.Target)(nil),
+		argmapper.Typed(t.ctx),
+	)
+	return m.(core.Target), err
 }
 
 func (t *targetIndexClient) Includes(uuid string) (exists bool, err error) {
-	return
+	incl, err := t.client.Includes(
+		t.ctx,
+		&vagrant_plugin_sdk.TargetIndex_IncludesRequest{Uuid: uuid},
+	)
+	return incl.Exists, err
 }
 
-func (t *targetIndexClient) Set(entry core.Machine) (updatedEntry core.Machine, err error) {
-	return
+func (t *targetIndexClient) Set(entry core.Target) (updatedEntry core.Target, err error) {
+	targetArg, err := t.Map(
+		entry,
+		(*vagrant_plugin_sdk.Args_Target)(nil),
+		argmapper.Typed(t.ctx),
+	)
+	if err != nil {
+		return nil, err
+	}
+	targetOut, err := t.client.Set(t.ctx, targetArg.(*vagrant_plugin_sdk.Args_Target))
+	if err != nil {
+		return nil, err
+	}
+	m, err := t.Map(
+		targetOut,
+		(*core.Target)(nil),
+		argmapper.Typed(t.ctx),
+	)
+	return m.(core.Target), err
 }
 
-func (t *targetIndexClient) Recover(entry core.Machine) (updatedEntry core.Machine, err error) {
-	return
+func (t *targetIndexClient) Recover(entry core.Target) (updatedEntry core.Target, err error) {
+	targetArg, err := t.Map(
+		entry,
+		(*vagrant_plugin_sdk.Args_Target)(nil),
+		argmapper.Typed(t.ctx),
+	)
+	if err != nil {
+		return nil, err
+	}
+	targetOut, err := t.client.Recover(t.ctx, targetArg.(*vagrant_plugin_sdk.Args_Target))
+	if err != nil {
+		return nil, err
+	}
+	m, err := t.Map(
+		targetOut,
+		(*core.Target)(nil),
+		argmapper.Typed(t.ctx),
+	)
+	return m.(core.Target), err
 }
 
 func (t *targetIndexServer) Delete(
 	ctx context.Context,
-	in *vagrant_plugin_sdk.Ref_Target,
+	in *vagrant_plugin_sdk.Args_Target,
 ) (empty *empty.Empty, err error) {
+	tar, err := t.Map(in, (*core.Target)(nil),
+		argmapper.Typed(ctx))
+	err = t.Impl.Delete(tar.(core.Target))
 	return
 }
 
@@ -101,13 +159,29 @@ func (t *targetIndexServer) Get(
 	ctx context.Context,
 	in *vagrant_plugin_sdk.TargetIndex_GetRequest,
 ) (target *vagrant_plugin_sdk.Args_Target, err error) {
-	return
+	tar, err := t.Impl.Get(in.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	result, err := t.Map(tar, (**vagrant_plugin_sdk.Args_Target)(nil))
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*vagrant_plugin_sdk.Args_Target), nil
 }
 
 func (t *targetIndexServer) Includes(
 	ctx context.Context,
 	in *vagrant_plugin_sdk.TargetIndex_IncludesRequest,
-) (target *vagrant_plugin_sdk.Args_Target, err error) {
+) (result *vagrant_plugin_sdk.TargetIndex_IncludesResponse, err error) {
+	resp, err := t.Impl.Includes(in.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	result = &vagrant_plugin_sdk.TargetIndex_IncludesResponse{
+		Exists: resp,
+	}
 	return
 }
 
@@ -115,14 +189,38 @@ func (t *targetIndexServer) Set(
 	ctx context.Context,
 	in *vagrant_plugin_sdk.Args_Target,
 ) (target *vagrant_plugin_sdk.Args_Target, err error) {
-	return
+	targetIn, err := t.Map(in, (*core.Target)(nil),
+		argmapper.Typed(ctx))
+
+	targetOut, err := t.Impl.Set(targetIn.(core.Target))
+	if err != nil {
+		return nil, err
+	}
+	result, err := t.Map(targetOut, (**vagrant_plugin_sdk.Args_Target)(nil))
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*vagrant_plugin_sdk.Args_Target), nil
 }
 
 func (t *targetIndexServer) Recover(
 	ctx context.Context,
 	in *vagrant_plugin_sdk.Args_Target,
 ) (target *vagrant_plugin_sdk.Args_Target, err error) {
-	return
+	targetIn, err := t.Map(in, (*core.Target)(nil),
+		argmapper.Typed(ctx))
+
+	targetOut, err := t.Impl.Recover(targetIn.(core.Target))
+	if err != nil {
+		return nil, err
+	}
+	result, err := t.Map(targetOut, (**vagrant_plugin_sdk.Args_Target)(nil))
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*vagrant_plugin_sdk.Args_Target), nil
 }
 
 var (
