72844bbd26952495570c6c6d8b9c22982941304b
Chris Roberts <croberts@hashicorp.com>
2021-08-05 11:40:51 -0700

    Enable argmapper output via environment variable

--- internal-shared/dynamic/logger.go
+++ internal-shared/dynamic/logger.go
@@ -6,10 +6,18 @@ import (
 	"github.com/hashicorp/go-hclog"
 )
 
-var Logger hclog.Logger = hclog.New(&hclog.LoggerOptions{
-	Name:       "vagrant.plugin.argmapper",
-	Level:      hclog.Error,
-	Output:     os.Stderr,
-	Color:      hclog.AutoColor,
-	JSONFormat: false,
-})
+func init() {
+	level := hclog.Error
+	if os.Getenv("VAGRANT_LOG_ARGMAPPER") != "" {
+		level = hclog.Trace
+	}
+	Logger = hclog.New(&hclog.LoggerOptions{
+		Name:       "vagrant.plugin.argmapper",
+		Level:      level,
+		Output:     os.Stderr,
+		Color:      hclog.AutoColor,
+		JSONFormat: false,
+	})
+}
+
+var Logger hclog.Logger
