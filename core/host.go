94c68424cb20599d06228a454d9df6b950cefc47
sophia <scastellarin95@gmail.com>
2021-09-22 15:28:56 -0500

    Pass statebag to host detect function

--- core/host.go
+++ core/host.go
@@ -8,7 +8,7 @@ type Host interface {
 	// Config() interface{}
 	// Documentation() (*docs.Documentation, error)
 	Capability(name string, args ...interface{}) (interface{}, error)
-	Detect() (bool, error)
+	Detect(state StateBag) (bool, error)
 	HasCapability(name string) (bool, error)
 	Parents() ([]string, error)
 
