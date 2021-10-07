d867ffb325f074890036815d4610ea25000f7677
Chris Roberts <croberts@hashicorp.com>
2021-10-07 14:39:23 -0700

    Fix server capability implementation to pass value and map correctly

--- internal/plugin/capability_platform.go
+++ internal/plugin/capability_platform.go
@@ -3,21 +3,25 @@ package plugin
 import (
 	"context"
 
-	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/LK4D4/joincontext"
 	"github.com/hashicorp/go-argmapper"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
+
 	"github.com/hashicorp/vagrant-plugin-sdk/component"
 	"github.com/hashicorp/vagrant-plugin-sdk/internal-shared/dynamic"
 	"github.com/hashicorp/vagrant-plugin-sdk/internal/funcspec"
 	"github.com/hashicorp/vagrant-plugin-sdk/proto/vagrant_plugin_sdk"
-	"google.golang.org/grpc"
-	"google.golang.org/protobuf/types/known/anypb"
 )
 
 type capabilityPlatform interface {
 	HasCapability(ctx context.Context, in *vagrant_plugin_sdk.FuncSpec_Args, opts ...grpc.CallOption) (*vagrant_plugin_sdk.Platform_Capability_CheckResp, error)
-	HasCapabilitySpec(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*vagrant_plugin_sdk.FuncSpec, error)
+	HasCapabilitySpec(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*vagrant_plugin_sdk.FuncSpec, error)
 	Capability(ctx context.Context, in *vagrant_plugin_sdk.Platform_Capability_NamedRequest, opts ...grpc.CallOption) (*vagrant_plugin_sdk.Platform_Capability_Resp, error)
 	CapabilitySpec(ctx context.Context, in *vagrant_plugin_sdk.Platform_Capability_NamedRequest, opts ...grpc.CallOption) (*vagrant_plugin_sdk.FuncSpec, error)
+	Parents(ctx context.Context, in *vagrant_plugin_sdk.FuncSpec_Args, opts ...grpc.CallOption) (*vagrant_plugin_sdk.Platform_ParentsResp, error)
+	ParentsSpec(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*vagrant_plugin_sdk.FuncSpec, error)
 }
 
 type capabilityClient struct {
@@ -30,7 +34,7 @@ type CapabilityArguments struct {
 }
 
 func (c *capabilityClient) HasCapabilityFunc() interface{} {
-	spec, err := c.client.HasCapabilitySpec(c.ctx, &empty.Empty{})
+	spec, err := c.client.HasCapabilitySpec(c.ctx, &emptypb.Empty{})
 	if err != nil {
 		return funcErr(err)
 	}
@@ -81,7 +85,38 @@ func (c *capabilityClient) CapabilityFunc(name string) interface{} {
 		if err != nil {
 			return nil, err
 		}
-		return resp.Result, nil
+
+		// Result will be returned as an Any so decode it
+		_, val, err := dynamic.DecodeAny(resp.Result)
+		if err != nil {
+			return nil, err
+		}
+
+		// Start by attempting to map the decoded value using
+		// the well known type protos
+		result, err := dynamic.MapFromWellKnownProto(val.(proto.Message))
+		if err != nil {
+			// And the actual result can be pretty much anything, so
+			// attempt to map it into something usable. If we can't
+			// map it, log the mapping failure but then just return
+			// the decoded value
+			result, err = dynamic.BlindMap(val, c.Mappers,
+				argmapper.Typed(c.internal()),
+				argmapper.Typed(c.ctx),
+				argmapper.Typed(c.Logger),
+			)
+
+			if err != nil {
+				c.Logger.Debug("failed to map decoded result from capability",
+					"value", val,
+					"error", err,
+				)
+
+				return val, err
+			}
+		}
+
+		return result, nil
 	}
 	return c.generateFunc(spec, cb)
 }
@@ -89,38 +124,11 @@ func (c *capabilityClient) CapabilityFunc(name string) interface{} {
 func (c *capabilityClient) Capability(name string, args ...interface{}) (interface{}, error) {
 	f := c.CapabilityFunc(name)
 
-	raw, err := c.callDynamicFunc(f, false,
+	return c.callDynamicFunc(f, false,
 		argmapper.Typed(args...),
 		argmapper.Typed(&CapabilityArguments{Arguments: args}),
 		argmapper.Typed(c.ctx),
 	)
-	if err != nil {
-		return nil, err
-	}
-
-	// Result will be returned as an Any so decode it
-	_, val, err := dynamic.DecodeAny(raw.(*anypb.Any))
-
-	// And the actual result can be pretty much anything, so
-	// attempt to map it into something usable. If we can't
-	// map it, log the mapping failure but then just return
-	// the decoded value
-	result, err := dynamic.UnknownMap(val, (*interface{})(nil), c.Mappers,
-		argmapper.Typed(c.internal()),
-		argmapper.Typed(c.ctx),
-		argmapper.Typed(c.Logger),
-	)
-
-	if err != nil {
-		c.Logger.Debug("failed to map decoded result from capability",
-			"value", val,
-			"error", err,
-		)
-
-		return val, err
-	}
-
-	return result, nil
 }
 
 type capabilityServer struct {
@@ -131,7 +139,7 @@ type capabilityServer struct {
 
 func (s *capabilityServer) HasCapabilitySpec(
 	ctx context.Context,
-	_ *empty.Empty,
+	_ *emptypb.Empty,
 ) (*vagrant_plugin_sdk.FuncSpec, error) {
 	if err := isImplemented(s, s.typ); err != nil {
 		return nil, err
@@ -170,14 +178,35 @@ func (s *capabilityServer) Capability(
 	ctx context.Context,
 	args *vagrant_plugin_sdk.Platform_Capability_NamedRequest,
 ) (*vagrant_plugin_sdk.Platform_Capability_Resp, error) {
-	_, err := s.callDynamicFunc(s.CapabilityImpl.CapabilityFunc(args.Name), false,
+	v, err := s.callDynamicFunc(s.CapabilityImpl.CapabilityFunc(args.Name), false,
 		args.FuncArgs.Args, argmapper.Typed(ctx))
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &vagrant_plugin_sdk.Platform_Capability_Resp{}, nil
+	var val interface{}
+	val, err = dynamic.MapToWellKnownProto(v)
+	if err != nil {
+		val, err = dynamic.UnknownMap(v, (*proto.Message)(nil), s.Mappers,
+			argmapper.Typed(s.internal()),
+			argmapper.Typed(ctx),
+			argmapper.Typed(s.Logger),
+		)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	result, err := dynamic.EncodeAny(val.(proto.Message))
+	if err != nil {
+		return nil, err
+	}
+
+	return &vagrant_plugin_sdk.Platform_Capability_Resp{
+		Result: result,
+	}, nil
 }
 
 var (
