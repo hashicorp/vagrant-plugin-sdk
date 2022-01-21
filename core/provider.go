1a1a4da61c041b46e56c2dd919900109f2a0bbc1
sophia <scastellarin95@gmail.com>
2022-01-21 11:07:28 -0600

    Fill in provider plugin impl

--- core/provider.go
+++ core/provider.go
@@ -10,6 +10,9 @@ type SshInfo struct {
 type Provider interface {
 	Usable() (bool, error)
 	Installed() (bool, error)
+	// TODO: Not sure about this init bit. Carrying it over here from the ruby
+	// side for consistency. But it would be cool if all plugins had an init
+	// type mechanism.
 	Init(Machine) error
 	Action(name string, args ...interface{}) error
 	MachineIdChanged() error
--- internal/plugin/provider.go
+++ internal/plugin/provider.go
@@ -2,10 +2,7 @@ package plugin
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes"
 	"github.com/hashicorp/go-argmapper"
 	"github.com/hashicorp/go-plugin"
 	"google.golang.org/grpc"
@@ -28,9 +25,15 @@ type ProviderPlugin struct {
 }
 
 func (p *ProviderPlugin) GRPCServer(broker *plugin.GRPCBroker, s *grpc.Server) error {
+	bs := p.NewServer(broker, p.Impl)
 	vagrant_plugin_sdk.RegisterProviderServiceServer(s, &providerServer{
 		Impl:       p.Impl,
-		BaseServer: p.NewServer(broker, p.Impl),
+		BaseServer: bs,
+		capabilityServer: &capabilityServer{
+			BaseServer:     bs,
+			CapabilityImpl: p.Impl,
+			typ:            "guest",
+		},
 	})
 	return nil
 }
@@ -40,18 +43,27 @@ func (p *ProviderPlugin) GRPCClient(
 	broker *plugin.GRPCBroker,
 	c *grpc.ClientConn,
 ) (interface{}, error) {
-	cl := vagrant_plugin_sdk.NewProviderServiceClient(c)
+	client := vagrant_plugin_sdk.NewProviderServiceClient(c)
+	bc := p.NewClient(ctx, broker, client.(SeederClient))
 	return &providerClient{
-		client:     cl,
-		BaseClient: p.NewClient(ctx, broker, cl.(SeederClient)),
+		client:     client,
+		BaseClient: bc,
+		capabilityClient: &capabilityClient{
+			client:     client,
+			BaseClient: bc,
+		},
 	}, nil
 }
 
 // providerClient is an implementation of component.Provider over gRPC.
 type providerClient struct {
 	*BaseClient
-
+	*capabilityClient
 	client vagrant_plugin_sdk.ProviderServiceClient
+}
+
+func (c *providerClient) GetCapabilityClient() *capabilityClient {
+	return c.capabilityClient
 }
 
 func (c *providerClient) Config() (interface{}, error) {
@@ -151,41 +163,129 @@ func (c *providerClient) Installed() (bool, error) {
 	return raw.(bool), nil
 }
 
-func (c *providerClient) ActionUpFunc() interface{} {
-	spec, err := c.client.ActionUpSpec(c.Ctx, &emptypb.Empty{})
+func (c *providerClient) ActionFunc(name string) interface{} {
+	spec, err := c.client.ActionSpec(c.Ctx, &vagrant_plugin_sdk.Provider_ActionRequest{
+		Name: name,
+	})
 	if err != nil {
 		return funcErr(err)
 	}
 	spec.Result = nil
-	cb := func(ctx context.Context, args funcspec.Args) (interface{}, error) {
-		resp, err := c.client.ActionUp(ctx, &vagrant_plugin_sdk.FuncSpec_Args{Args: args})
-		if err != nil {
-			return false, err
-		}
-		return resp.Result, nil
+	cb := func(ctx context.Context, args funcspec.Args) error {
+		_, err := c.client.Action(ctx, &vagrant_plugin_sdk.Provider_ActionRequest{
+			Name:     name,
+			FuncArgs: &vagrant_plugin_sdk.FuncSpec_Args{Args: args},
+		})
+		return err
+	}
+	return c.GenerateFunc(spec, cb)
+	return nil
+}
+
+func (c *providerClient) Action(name string, args ...interface{}) error {
+	f := c.ActionFunc(name)
+	_, err := c.CallDynamicFunc(f, false,
+		argmapper.Typed(&component.Direct{Arguments: args}),
+		argmapper.Typed(args...),
+		argmapper.Typed(c.Ctx),
+	)
+	return err
+}
+
+func (c *providerClient) MachineIdChangedFunc() interface{} {
+	spec, err := c.client.MachineIdChangedSpec(c.Ctx, &emptypb.Empty{})
+	if err != nil {
+		return funcErr(err)
+	}
+	spec.Result = nil
+	cb := func(ctx context.Context, args funcspec.Args) error {
+		_, err := c.client.MachineIdChanged(ctx, &vagrant_plugin_sdk.FuncSpec_Args{Args: args})
+		return err
 	}
 	return c.GenerateFunc(spec, cb)
 }
 
-func (c *providerClient) ActionUp() error {
-	f := c.ActionUpFunc()
+func (c *providerClient) MachineIdChanged() error {
+	f := c.MachineIdChangedFunc()
 	_, err := c.CallDynamicFunc(f, false,
 		argmapper.Typed(c.Ctx),
 	)
+	return err
+}
+
+func (c *providerClient) SshInfoFunc() interface{} {
+	spec, err := c.client.SshInfoSpec(c.Ctx, &emptypb.Empty{})
 	if err != nil {
-		return err
+		return funcErr(err)
+	}
+	spec.Result = nil
+	cb := func(ctx context.Context, args funcspec.Args) (*core.SshInfo, error) {
+		resp, err := c.client.SshInfo(ctx, &vagrant_plugin_sdk.FuncSpec_Args{Args: args})
+		if err != nil {
+			return nil, err
+		}
+		result, err := c.Map(resp, (**core.SshInfo)(nil),
+			argmapper.Typed(c.Ctx))
+		if err != nil {
+			return nil, err
+		}
+		return result.(*core.SshInfo), nil
+	}
+	return c.GenerateFunc(spec, cb)
+}
+
+func (c *providerClient) SshInfo() (*core.SshInfo, error) {
+	f := c.SshInfoFunc()
+	raw, err := c.CallDynamicFunc(f, (**core.SshInfo)(nil),
+		argmapper.Typed(c.Ctx),
+	)
+	if err != nil {
+		return nil, err
 	}
 
-	return nil
+	return raw.(*core.SshInfo), nil
+}
+
+func (c *providerClient) StateFunc() interface{} {
+	spec, err := c.client.StateSpec(c.Ctx, &emptypb.Empty{})
+	if err != nil {
+		return funcErr(err)
+	}
+	spec.Result = nil
+	cb := func(ctx context.Context, args funcspec.Args) (*core.MachineState, error) {
+		resp, err := c.client.State(ctx, &vagrant_plugin_sdk.FuncSpec_Args{Args: args})
+		if err != nil {
+			return nil, err
+		}
+		result, err := c.Map(resp, (**core.MachineState)(nil),
+			argmapper.Typed(c.Ctx))
+		if err != nil {
+			return nil, err
+		}
+		return result.(*core.MachineState), nil
+	}
+	return c.GenerateFunc(spec, cb)
+}
+
+func (c *providerClient) State() (*core.MachineState, error) {
+	f := c.StateFunc()
+	raw, err := c.CallDynamicFunc(f, (**core.MachineState)(nil),
+		argmapper.Typed(c.Ctx),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return raw.(*core.MachineState), nil
 }
 
 // providerServer is a gRPC server that the client talks to and calls a
 // real implementation of the component.
 type providerServer struct {
 	*BaseServer
+	*capabilityServer
 
 	Impl component.Provider
-	vagrant_plugin_sdk.UnsafeProviderServiceServer
 }
 
 func (s *providerServer) ConfigStruct(
@@ -261,39 +361,6 @@ func (s *providerServer) Installed(
 		IsInstalled: raw.(bool)}, nil
 }
 
-func (s *providerServer) ActionUpSpec(
-	ctx context.Context,
-	args *emptypb.Empty,
-) (*vagrant_plugin_sdk.FuncSpec, error) {
-	if err := isImplemented(s, "provider"); err != nil {
-		return nil, err
-	}
-
-	return s.GenerateSpec(s.Impl.ActionUpFunc())
-}
-
-func (s *providerServer) ActionUp(
-	ctx context.Context,
-	args *vagrant_plugin_sdk.FuncSpec_Args,
-) (*vagrant_plugin_sdk.Provider_ActionResp, error) {
-	raw, err := s.CallDynamicFunc(s.Impl.ActionUpFunc(), (*proto.Message)(nil),
-		args.Args, argmapper.Typed(ctx))
-
-	if err != nil {
-		return nil, err
-	}
-	// Expect the results to be proto.Messages
-	msg, ok := raw.(proto.Message)
-	if !ok {
-		return nil, fmt.Errorf(
-			"result of plugin-based function must be a proto.Message, got %T", msg)
-	}
-	anyVal, err := ptypes.MarshalAny(msg)
-
-	// TODO: This maybe needs to be expanded
-	return &vagrant_plugin_sdk.Provider_ActionResp{Result: anyVal}, nil
-}
-
 func (s *providerServer) InitSpec(
 	ctx context.Context,
 	args *emptypb.Empty,
@@ -317,6 +384,128 @@ func (s *providerServer) Init(
 	}
 
 	return &emptypb.Empty{}, nil
+}
+
+func (s *providerServer) ActionSpec(
+	ctx context.Context,
+	args *vagrant_plugin_sdk.Provider_ActionRequest,
+) (*vagrant_plugin_sdk.FuncSpec, error) {
+	if err := isImplemented(s, "provider"); err != nil {
+		return nil, err
+	}
+
+	return s.GenerateSpec(s.Impl.ActionFunc(args.Name))
+}
+
+func (s *providerServer) Action(
+	ctx context.Context,
+	args *vagrant_plugin_sdk.Provider_ActionRequest,
+) (*emptypb.Empty, error) {
+	_, err := s.CallDynamicFunc(
+		s.Impl.ActionFunc(args.Name),
+		false,
+		args.FuncArgs.Args,
+		argmapper.Typed(ctx),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *providerServer) MachineIdChangedSpec(
+	ctx context.Context,
+	args *emptypb.Empty,
+) (*vagrant_plugin_sdk.FuncSpec, error) {
+	if err := isImplemented(s, "provider"); err != nil {
+		return nil, err
+	}
+
+	return s.GenerateSpec(s.Impl.MachineIdChangedFunc())
+}
+
+func (s *providerServer) MachineIdChanged(
+	ctx context.Context,
+	args *vagrant_plugin_sdk.FuncSpec_Args,
+) (*emptypb.Empty, error) {
+	_, err := s.CallDynamicFunc(s.Impl.MachineIdChangedFunc(), false,
+		args.Args, argmapper.Typed(ctx))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *providerServer) SshInfoSpec(
+	ctx context.Context,
+	args *emptypb.Empty,
+) (*vagrant_plugin_sdk.FuncSpec, error) {
+	if err := isImplemented(s, "provider"); err != nil {
+		return nil, err
+	}
+
+	return s.GenerateSpec(s.Impl.SshInfoFunc())
+}
+
+func (s *providerServer) SshInfo(
+	ctx context.Context,
+	args *vagrant_plugin_sdk.FuncSpec_Args,
+) (*vagrant_plugin_sdk.SSHInfo, error) {
+	raw, err := s.CallDynamicFunc(s.Impl.SshInfoFunc(), (**core.SshInfo)(nil),
+		args.Args, argmapper.Typed(ctx))
+
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := s.Map(
+		raw,
+		(**vagrant_plugin_sdk.SSHInfo)(nil),
+		argmapper.Typed(ctx),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*vagrant_plugin_sdk.SSHInfo), nil
+}
+
+func (s *providerServer) StateSpec(
+	ctx context.Context,
+	args *emptypb.Empty,
+) (*vagrant_plugin_sdk.FuncSpec, error) {
+	if err := isImplemented(s, "provider"); err != nil {
+		return nil, err
+	}
+
+	return s.GenerateSpec(s.Impl.StateFunc())
+}
+
+func (s *providerServer) State(
+	ctx context.Context,
+	args *vagrant_plugin_sdk.FuncSpec_Args,
+) (*vagrant_plugin_sdk.Args_Target_Machine_State, error) {
+	raw, err := s.CallDynamicFunc(s.Impl.StateFunc(), (**core.MachineState)(nil),
+		args.Args, argmapper.Typed(ctx))
+
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := s.Map(
+		raw,
+		(**vagrant_plugin_sdk.Args_Target_Machine_State)(nil),
+		argmapper.Typed(ctx),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*vagrant_plugin_sdk.Args_Target_Machine_State), nil
 }
 
 var (
