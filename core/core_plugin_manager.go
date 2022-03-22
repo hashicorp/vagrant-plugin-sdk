8b4f242af6b9e8c6ce0d6dc8f52ea780cca2d193
sophia <scastellarin95@gmail.com>
2022-03-22 16:22:23 -0500

    Add core component for a core plugin manager

---
+++ core/core_plugin_manager.go
@@ -0,0 +1,38 @@
+package core
+
+// Type is an enum of all the types of core components supported.
+type Type uint
+
+const (
+	InvalidType Type = iota // Invalid
+	BasisType
+	BoxCollectionType
+	BoxMetadataType
+	BoxType
+	MachineType
+	PluginManagerType
+	ProjectType
+	StateBagType
+	TargetIndexType
+	TargetType
+	maxType
+)
+
+// TypeMap is a mapping of Type to the nil pointer to the interface of that
+// type. This can be used with libraries such as mapper.
+var TypeMap = map[Type]interface{}{
+	BasisType:         (*Basis)(nil),
+	BoxCollectionType: (*BoxCollection)(nil),
+	BoxMetadataType:   (*BoxMetadata)(nil),
+	BoxType:           (*Box)(nil),
+	MachineType:       (*Machine)(nil),
+	PluginManagerType: (*PluginManager)(nil),
+	ProjectType:       (*Project)(nil),
+	StateBagType:      (*StateBag)(nil),
+	TargetIndexType:   (*TargetIndex)(nil),
+	TargetType:        (*Target)(nil),
+}
+
+type CorePluginManager interface {
+	GetPlugin(pluginType Type) (interface{}, error)
+}
