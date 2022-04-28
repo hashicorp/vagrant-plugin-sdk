7c4f9aeb59a875ea400a536b05654a6a5e993583
sophia <scastellarin95@gmail.com>
2022-04-28 12:59:29 -0500

    Add beginning of localizer package

---
+++ localizer/localizer.go
@@ -0,0 +1,43 @@
+package localizer
+
+import (
+	"encoding/json"
+
+	"github.com/hashicorp/vagrant-plugin-sdk/terminal"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+)
+
+type Localizer struct {
+	localizer *i18n.Localizer
+	bundle    *i18n.Bundle
+	terminal  terminal.UI
+}
+
+func NewLocalizer(lang string, terminal terminal.UI) (localizer *Localizer, err error) {
+	if lang == "" {
+		lang = language.English.String()
+	}
+	bundle := i18n.NewBundle(language.English)
+	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+	_, err = bundle.LoadMessageFile("locales/en.json")
+	if err != nil {
+		return nil, err
+	}
+	l := i18n.NewLocalizer(bundle, lang)
+	return &Localizer{
+		localizer: l, bundle: bundle, terminal: terminal,
+	}, nil
+}
+
+func (l *Localizer) LocalizeMsg(msg string) error {
+	config := i18n.LocalizeConfig{
+		MessageID: msg,
+	}
+	localizedMsg, err := l.localizer.Localize(&config)
+	if err != nil {
+		return err
+	}
+	l.terminal.Output(localizedMsg)
+	return nil
+}
