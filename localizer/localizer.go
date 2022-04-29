7dca1f327387936f7643f75918a7607117296564
sophia <scastellarin95@gmail.com>
2022-04-29 15:04:28 -0500

    Create localizer with custon translations

--- localizer/localizer.go
+++ localizer/localizer.go
@@ -14,28 +14,46 @@ type Localizer struct {
 	terminal  terminal.UI
 }
 
-func NewLocalizer(lang string, terminal terminal.UI) (localizer *Localizer, err error) {
-	if lang == "" {
-		lang = language.English.String()
-	}
-	bundle := i18n.NewBundle(language.English)
+func NewPluginLocalizer(lang language.Tag, localeData []byte, localePath string, terminal terminal.UI) (localizer *Localizer, err error) {
+	bundle := i18n.NewBundle(lang)
 	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
-	localDataPath := "localizer/locales/" + lang + ".json"
-	enData, err := Asset(localDataPath)
+	_, err = bundle.ParseMessageFileBytes(localeData, localePath)
 	if err != nil {
 		return nil, err
 	}
-	_, err = bundle.ParseMessageFileBytes(enData, localDataPath)
-	if err != nil {
-		return nil, err
-	}
-	l := i18n.NewLocalizer(bundle, lang)
+	l := i18n.NewLocalizer(bundle, lang.String())
 	return &Localizer{
 		localizer: l, bundle: bundle, terminal: terminal,
 	}, nil
 }
 
-func (l *Localizer) LocalizeMsg(msg string, templateData interface{}) error {
+func NewCoreLocalizer(lang language.Tag, terminal terminal.UI) (localizer *Localizer, err error) {
+	bundle := i18n.NewBundle(lang)
+	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+	localDataPath := "localizer/locales/" + lang.String() + ".json"
+	localeData, err := Asset(localDataPath)
+	if err != nil {
+		return nil, err
+	}
+	_, err = bundle.ParseMessageFileBytes(localeData, localDataPath)
+	if err != nil {
+		return nil, err
+	}
+	l := i18n.NewLocalizer(bundle, lang.String())
+	return &Localizer{
+		localizer: l, bundle: bundle, terminal: terminal,
+	}, nil
+}
+
+func (l *Localizer) LocalizeMsg(msg string, templateData interface{}) (string, error) {
+	config := i18n.LocalizeConfig{
+		MessageID:    msg,
+		TemplateData: templateData,
+	}
+	return l.localizer.Localize(&config)
+}
+
+func (l *Localizer) Output(msg string, templateData interface{}) error {
 	config := i18n.LocalizeConfig{
 		MessageID:    msg,
 		TemplateData: templateData,
