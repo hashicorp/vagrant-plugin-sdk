7139bfa0a088f3b2f31188faeecd64a68928d46a
sophia <scastellarin95@gmail.com>
2022-05-03 16:24:59 -0500

    Add basic test for searching VagrantCloud

---
+++ cloud/cloud_test.go
@@ -0,0 +1,14 @@
+package cloud
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSearch(t *testing.T) {
+	vcc := TestUnauthedClient(t)
+	res, err := vcc.Seach("hashicorp", "", "", "", 10, 1)
+	require.NoError(t, err)
+	require.Len(t, res["boxes"], 10)
+}
---
+++ cloud/testing_client.go
@@ -0,0 +1,16 @@
+package cloud
+
+import (
+	"github.com/mitchellh/go-testing-interface"
+	"github.com/stretchr/testify/require"
+)
+
+// TestUnauthedClient returns a fully in-memory and side-effect free VagrantCloudClient
+// that does not have any auth tokens.
+func TestUnauthedClient(t testing.T) *VagrantCloudClient {
+	vcc, err := NewVagrantCloudClient(
+		"", DEFAULT_RETRY_COUNT, DEFAULT_RETRY_INTERVAL, DEFAULT_URL,
+	)
+	require.NoError(t, err)
+	return vcc
+}
