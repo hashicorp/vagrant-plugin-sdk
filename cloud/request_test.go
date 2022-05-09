7c73f5eba2441fa615cda9914495b1fbe5ec50a2
sophia <scastellarin95@gmail.com>
2022-05-09 13:13:05 -0500

    Output warning when url targets differ from vagrant cloud

--- cloud/request_test.go
+++ cloud/request_test.go
@@ -1,6 +1,8 @@
 package cloud
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,6 +10,7 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/hashicorp/vagrant-plugin-sdk/terminal"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,6 +24,19 @@ func validateRequestServer(t *testing.T, validate func(t *testing.T, r *http.Req
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		validate(t, r)
 	}))
+}
+
+type TestingUI struct {
+	terminal.UI
+	buf bytes.Buffer
+}
+
+func NewTestingUI(buf bytes.Buffer) *TestingUI {
+	return &TestingUI{UI: terminal.NonInteractiveUI(context.Background()), buf: buf}
+}
+
+func (ui *TestingUI) Output(msg string, raw ...interface{}) {
+	ui.UI.Output(msg, terminal.WithWriter(&ui.buf), raw)
 }
 
 func TestNewVagrantCloudRequest(t *testing.T) {
@@ -69,10 +85,27 @@ func TestWarnDifferentURL(t *testing.T) {
 	require.NoError(t, err)
 	vcr, err := NewVagrantCloudRequest(
 		WithURL(ts.URL),
-		WarnDifferentTarget(testServerURL),
+		WarnDifferentTarget(testServerURL, nil),
 	)
 	require.NoError(t, err)
 	raw, err := vcr.Do()
+	require.NoError(t, err)
+	require.NotNil(t, raw)
+	require.Contains(t, string(raw), "Hello, client")
+	ts.Close()
+
+	ts = simpleServer()
+	testServerURL, err = url.Parse(ts.URL)
+	require.NoError(t, err)
+	var buf bytes.Buffer
+	ui := NewTestingUI(buf)
+	vcr, err = NewVagrantCloudRequest(
+		WithURL(ts.URL),
+		WarnDifferentTarget(testServerURL, ui),
+	)
+	require.NoError(t, err)
+	raw, err = vcr.Do()
+	require.Contains(t, ui.buf.String(), "Vagrant has detected a custom Vagrant server in use for downloading")
 	require.NoError(t, err)
 	require.NotNil(t, raw)
 	require.Contains(t, string(raw), "Hello, client")
