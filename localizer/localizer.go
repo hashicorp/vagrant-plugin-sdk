4f4e4579808155730ef51ea319447e15b468e595
sophia <scastellarin95@gmail.com>
2022-05-11 12:38:27 -0500

    Allow plugins to load multiple localization files

--- localizer/localizer.go
+++ localizer/localizer.go
@@ -2,11 +2,23 @@ package localizer
 
 import (
 	"encoding/json"
+	"os"
+	"strings"
 
 	"github.com/hashicorp/vagrant-plugin-sdk/terminal"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
 )
+
+const (
+	DEFAULT_LANGUAGE = "en"
+)
+
+type LocaleData struct {
+	LocaleData []byte
+	LocalePath string
+	Languages  []language.Tag
+}
 
 type Localizer struct {
 	localizer *i18n.Localizer
@@ -14,10 +26,12 @@ type Localizer struct {
 	terminal  terminal.UI
 }
 
-func NewPluginLocalizer(lang language.Tag, localeData []byte, localePath string, terminal terminal.UI) (localizer *Localizer, err error) {
-	bundle := i18n.NewBundle(lang)
-	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
-	_, err = bundle.ParseMessageFileBytes(localeData, localePath)
+func NewPluginLocalizer(terminal terminal.UI, data ...LocaleData) (localizer *Localizer, err error) {
+	lang, err := getLang()
+	if err != nil {
+		return nil, err
+	}
+	bundle, err := loadLocaleData(lang, data...)
 	if err != nil {
 		return nil, err
 	}
@@ -27,15 +41,21 @@ func NewPluginLocalizer(lang language.Tag, localeData []byte, localePath string,
 	}, nil
 }
 
-func NewCoreLocalizer(lang language.Tag, terminal terminal.UI) (localizer *Localizer, err error) {
-	bundle := i18n.NewBundle(lang)
-	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+func NewCoreLocalizer(terminal terminal.UI) (localizer *Localizer, err error) {
+	lang, err := getLang()
+	if err != nil {
+		return nil, err
+	}
 	localDataPath := "localizer/locales/" + lang.String() + ".json"
 	localeData, err := Asset(localDataPath)
 	if err != nil {
 		return nil, err
 	}
-	_, err = bundle.ParseMessageFileBytes(localeData, localDataPath)
+	bundle, err := loadLocaleData(lang, LocaleData{
+		LocaleData: localeData,
+		LocalePath: localDataPath,
+		Languages:  []language.Tag{language.English, language.AmericanEnglish, language.BritishEnglish},
+	})
 	if err != nil {
 		return nil, err
 	}
@@ -64,4 +84,38 @@ func (l *Localizer) Output(msg string, templateData interface{}) error {
 	}
 	l.terminal.Output(localizedMsg)
 	return nil
+}
+
+func getLang() (locale language.Tag, err error) {
+	// TODO: make this work for Windows too
+	lang := os.Getenv("LC_ALL")
+	if lang == "" {
+		langVar := os.Getenv("LANG")
+		// LANG returns the locale and encoding. Extract just the locale
+		lang = strings.Split(langVar, ".")[0]
+	}
+	if lang == "" {
+		lang = DEFAULT_LANGUAGE
+	}
+	return language.Parse(lang)
+}
+
+func loadLocaleData(lang language.Tag, data ...LocaleData) (bundle *i18n.Bundle, err error) {
+	bundle = i18n.NewBundle(lang)
+	for _, d := range data {
+		msg, err := i18n.ParseMessageFileBytes(
+			d.LocaleData,
+			d.LocalePath,
+			map[string]i18n.UnmarshalFunc{"json": json.Unmarshal},
+		)
+		if err != nil {
+			return nil, err
+		}
+		for _, lang := range d.Languages {
+			if err := bundle.AddMessages(lang, msg.Messages...); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return
 }
