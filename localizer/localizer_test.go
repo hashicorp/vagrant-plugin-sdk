48161c1687fb236a8b6cf207be1c73f8b71c2cc5
sophia <scastellarin95@gmail.com>
2022-05-16 12:29:23 -0500

    Make localizer only localize messages and not output to ui
    Not including the ui package in the localizer package allows the ui
    package to also localize things like error messages. Otherwise,
    importing the localizer package from the ui package would cause a
    circular dependency error

--- localizer/localizer_test.go
+++ localizer/localizer_test.go
@@ -1,47 +1,24 @@
 package localizer
 
 import (
-	"bytes"
-	"context"
 	"encoding/json"
 	"os"
 	"testing"
 
-	"github.com/hashicorp/vagrant-plugin-sdk/terminal"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/text/language"
 )
-
-type testingUI struct {
-	terminal.UI
-	buf bytes.Buffer
-}
-
-func NewTestingUI(buf bytes.Buffer) *testingUI {
-	return &testingUI{UI: terminal.NonInteractiveUI(context.Background()), buf: buf}
-}
-
-func (ui *testingUI) Output(msg string, raw ...interface{}) {
-	ui.UI.Output(msg, terminal.WithWriter(&ui.buf), raw)
-}
 
 func TestNewCoreLocalizer(t *testing.T) {
 	oldLC_ALL := os.Getenv("LC_ALL")
 	os.Setenv("LC_ALL", "en")
 	defer os.Setenv("LC_ALL", oldLC_ALL)
 
-	var buf bytes.Buffer
-	ui := NewTestingUI(buf)
-
 	expectedMsg := "Adding box"
-	l, err := NewCoreLocalizer(ui)
+	l, err := NewCoreLocalizer()
 	require.NoError(t, err)
 	require.NotNil(t, l)
 	// Test output prints to terminal
-	err = l.Output("box_add", nil)
-	require.NoError(t, err)
-	require.Contains(t, ui.buf.String(), expectedMsg)
-	// Test localizing message
 	msg, err := l.LocalizeMsg("box_add", nil)
 	require.NoError(t, err)
 	require.Contains(t, msg, expectedMsg)
@@ -50,9 +27,6 @@ func TestNewCoreLocalizer(t *testing.T) {
 func TestNewPluginLocalizer(t *testing.T) {
 	oldLC_ALL := os.Getenv("LC_ALL")
 	defer os.Setenv("LC_ALL", oldLC_ALL)
-
-	var buf bytes.Buffer
-	ui := NewTestingUI(buf)
 
 	expectedMsgEN := "yes i am test data"
 	jsonStrEN, err := json.Marshal(map[string]string{
@@ -81,7 +55,7 @@ func TestNewPluginLocalizer(t *testing.T) {
 
 	os.Setenv("LC_ALL", "en")
 	// Test localizing message to english
-	l, err := NewPluginLocalizer(ui, localeData...)
+	l, err := NewPluginLocalizer(localeData...)
 	require.NoError(t, err)
 	msg, err := l.LocalizeMsg("testdata", nil)
 	require.NoError(t, err)
@@ -89,7 +63,7 @@ func TestNewPluginLocalizer(t *testing.T) {
 
 	os.Setenv("LC_ALL", "es")
 	// Test localizing message to spanish
-	l, err = NewPluginLocalizer(ui, localeData...)
+	l, err = NewPluginLocalizer(localeData...)
 	require.NoError(t, err)
 	msg, err = l.LocalizeMsg("testdata", nil)
 	require.NoError(t, err)
@@ -97,9 +71,6 @@ func TestNewPluginLocalizer(t *testing.T) {
 }
 
 func TestNewPluginLocalizerBadPath(t *testing.T) {
-	var buf bytes.Buffer
-	ui := NewTestingUI(buf)
-
 	expectedMsgEN := "yes i am test data"
 	jsonStrEN, err := json.Marshal(map[string]string{
 		"testdata": expectedMsgEN,
@@ -113,6 +84,6 @@ func TestNewPluginLocalizerBadPath(t *testing.T) {
 			Languages:  []language.Tag{language.English, language.AmericanEnglish},
 		},
 	}
-	_, err = NewPluginLocalizer(ui, localeData...)
+	_, err = NewPluginLocalizer(localeData...)
 	require.Error(t, err)
 }
