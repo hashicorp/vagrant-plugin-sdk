1996a438538e24e7a40c12c2377366a372107d1c
sophia <scastellarin95@gmail.com>
2022-05-27 10:48:04 -0500

    go generate

--- core/mocks/Downloader.go
+++ core/mocks/Downloader.go
@@ -9,6 +9,29 @@ type Downloader struct {
 	mock.Mock
 }
 
+// Config provides a mock function with given fields:
+func (_m *Downloader) Config() (interface{}, error) {
+	ret := _m.Called()
+
+	var r0 interface{}
+	if rf, ok := ret.Get(0).(func() interface{}); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(interface{})
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Download provides a mock function with given fields:
 func (_m *Downloader) Download() error {
 	ret := _m.Called()
