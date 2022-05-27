35fe120785b998836eae42c3e6cec1665a6c30d6
Paul Hinze <phinze@phinze.com>
2022-05-27 12:21:13 -0500

    Revert "Revert "Changes required to port default provider selection""

--- core/state.go
+++ core/state.go
@@ -10,3 +10,18 @@ const (
 	NOT_CREATED
 	PENDING
 )
+
+// IsActive tells whether the machine is in an "active" state. Active is
+// defined in legacy vagrant as "having an id file in the data dir" which is
+// roughtly equivalent to "has an entry in the underlying provider."
+func (s State) IsActive() bool {
+	switch s {
+	case CREATED, HALTED, PENDING:
+		return true
+	case UNKNOWN, DESTROYED, NOT_CREATED:
+		return false
+	default:
+		return false
+	}
+
+}
