6a4f39b6be858c1abe4969d380412785da81c6ff
Paul Hinze <phinze@phinze.com>
2022-06-10 13:19:28 -0500

    Extra logging to diagnose multiplying typed seeds

--- core/seeder.go
+++ core/seeder.go
@@ -1,5 +1,11 @@
 package core
 
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-hclog"
+)
+
 func NewSeeds() *Seeds {
 	return &Seeds{
 		Named: map[string]interface{}{},
@@ -12,17 +18,23 @@ type Seeds struct {
 	Typed []interface{}
 }
 
-func (s *Seeds) AddTyped(v ...interface{}) {
+func (s *Seeds) AddTyped(l hclog.Logger, v ...interface{}) {
+	log := l.Named("seed-saver")
 	// Generate map of existing values for quick filtering
 	exist := make(map[interface{}]struct{})
 	for _, t := range s.Typed {
 		exist[t] = struct{}{}
 	}
+	log.Debug("existing seeds", "exist", fmt.Sprintf("%#v", exist))
 
 	for _, t := range v {
+		log.Debug("attempting to add typed seed", "value", fmt.Sprintf("%#v", t))
 		if _, ok := exist[t]; !ok {
+			log.Debug("it does not exist so adding it")
 			s.Typed = append(s.Typed, t)
 			exist[t] = struct{}{}
+		} else {
+			log.Debug("it already exists so skipping it")
 		}
 	}
 }
