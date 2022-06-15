d49102cd8c0a5dbdc5e0ae04dc95cd76da8dad08
Chris Roberts <croberts@hashicorp.com>
2022-06-15 11:38:15 -0700

    Export CleanupFn type and remove todo

--- internal-shared/cleanup/cleanup.go
+++ internal-shared/cleanup/cleanup.go
@@ -1,36 +1,35 @@
 package cleanup
 
 import (
-	//	"fmt"
 	"sync"
 
 	"github.com/hashicorp/go-multierror"
 )
 
+type CleanupFn func() error
+
 // Cleanup is an interface for registering cleanup functions
 type Cleanup interface {
-	Do(cleanupFn) // registers a cleanup function
+	Do(CleanupFn) // registers a cleanup function
 	Close() error // executes registered cleanup functions
 }
 
 // Create a new cleanup instance
 func New() Cleanup {
 	return &cleanup{
-		fns: []cleanupFn{},
+		fns: []CleanupFn{},
 	}
 }
 
-type cleanupFn func() error
-
 type cleanup struct {
-	fns  []cleanupFn
+	fns  []CleanupFn
 	l    sync.Mutex
 	mark bool
 }
 
 // Register a new cleanup task to be performed on close
 // NOTE: cleanup tasks are only called once
-func (c *cleanup) Do(fn cleanupFn) {
+func (c *cleanup) Do(fn CleanupFn) {
 	c.l.Lock()
 	defer c.l.Unlock()
 
@@ -41,12 +40,6 @@ func (c *cleanup) Do(fn cleanupFn) {
 func (c *cleanup) Close() (err error) {
 	c.l.Lock()
 	defer c.l.Unlock()
-	// TODO(spox): Uncomment once closers are properly
-	//             setup to only be called once.
-	// if c.mark {
-	// 	return fmt.Errorf("Cleanup has already been closed")
-	// }
-
 	for _, fn := range c.fns {
 		e := fn()
 		if e != nil {
@@ -55,7 +48,7 @@ func (c *cleanup) Close() (err error) {
 	}
 
 	// Remove cleanup tasks as they have been called
-	c.fns = []cleanupFn{}
+	c.fns = []CleanupFn{}
 
 	// Set the mark to show close has been called
 	c.mark = true
