431f8fcff293106f901de399a9eec1be0301b853
Chris Roberts <croberts@hashicorp.com>
2022-07-08 09:11:52 -0700

    Add an `Append()` to the Cleanup interface

--- internal-shared/cleanup/cleanup.go
+++ internal-shared/cleanup/cleanup.go
@@ -10,21 +10,26 @@ type CleanupFn func() error
 
 // Cleanup is an interface for registering cleanup functions
 type Cleanup interface {
-	Prepend(CleanupFn) // registers a cleanup function at start of current cleanup stack
-	Do(CleanupFn)      // registers a cleanup function at the end of the current cleanup stack
+	Append(CleanupFn)  // registers a cleanup function to run after general stack
+	Prepend(CleanupFn) // registers a cleanup function to run before general stack
+	Do(CleanupFn)      // registers a cleanup function to run in the general stack
 	Close() error      // executes registered cleanup functions
 }
 
 // Create a new cleanup instance
 func New() Cleanup {
 	return &cleanup{
-		fns: []CleanupFn{},
+		aFns: []CleanupFn{},
+		fns:  []CleanupFn{},
+		pFns: []CleanupFn{},
 	}
 }
 
 type cleanup struct {
-	fns []CleanupFn
-	l   sync.Mutex
+	aFns []CleanupFn
+	fns  []CleanupFn
+	pFns []CleanupFn
+	l    sync.Mutex
 }
 
 // Register a new cleanup task to be performed on close
@@ -36,20 +41,34 @@ func (c *cleanup) Do(fn CleanupFn) {
 	c.fns = append(c.fns, fn)
 }
 
+func (c *cleanup) Append(fn CleanupFn) {
+	c.l.Lock()
+	defer c.l.Unlock()
+
+	c.aFns = append(c.aFns, fn)
+}
+
 func (c *cleanup) Prepend(fn CleanupFn) {
 	c.l.Lock()
 	defer c.l.Unlock()
 
-	fns := make([]CleanupFn, len(c.fns)+1)
-	fns[0] = fn
-	copy(fns[1:], c.fns)
-	c.fns = fns
+	c.pFns = append(c.pFns, fn)
 }
 
 // Run all cleanup tasks
 func (c *cleanup) Close() (err error) {
 	c.l.Lock()
 	defer c.l.Unlock()
+
+	// First run all tasks in the prepend collection
+	for _, fn := range c.pFns {
+		e := fn()
+		if e != nil {
+			err = multierror.Append(err, e)
+		}
+	}
+
+	// Next run all tasks in regular collection
 	for _, fn := range c.fns {
 		e := fn()
 		if e != nil {
@@ -57,7 +76,17 @@ func (c *cleanup) Close() (err error) {
 		}
 	}
 
+	// Finally run all tasks in append collection
+	for _, fn := range c.aFns {
+		e := fn()
+		if e != nil {
+			err = multierror.Append(err, e)
+		}
+	}
+
 	// Remove cleanup tasks as they have been called
+	c.aFns = []CleanupFn{}
+	c.pFns = []CleanupFn{}
 	c.fns = []CleanupFn{}
 
 	return
